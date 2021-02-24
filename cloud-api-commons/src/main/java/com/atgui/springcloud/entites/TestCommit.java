854754842442fcd5df86109d623cf0563e3a6b12
Wangtengfei525 <17828129479@163.com>
2021-02-24 12:50:02 +0800

    自定义工具包    2021/2/24的测试提交

---
+++ cloud-api-commons/src/main/java/com/atgui/springcloud/entites/TestCommit.java
@@ -0,0 +1,31 @@
+package com.atgui.springcloud.entites;
+
+public class TestCommit {
+    private  String name;
+    private  int age;
+    private   double   totalMoney;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getTotalMoney() {
+        return totalMoney;
+    }
+
+    public void setTotalMoney(double totalMoney) {
+        this.totalMoney = totalMoney;
+    }
+}
